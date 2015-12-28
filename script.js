dd955f9d4fbf58462a123a4d93299d2f5b95d3e8
Akeem Spencer <Akeem@Akeems-MBP.home>
2015-12-28 00:47:58 -0500

    animation functions added for scrolling purposes on second nav.

--- script.js
+++ script.js
@@ -9,4 +9,27 @@ $(document).ready(function(){
 		}
 	);
 
+	$("button").on('click', function(){
+		var button_id = $(this).attr('id');
+		console.log(button_id);
+		if (button_id == ar_l){
+			PushNavLeft();
+		}
+		else
+			return console.log('false');
+	});
+
+	 function PushNavLeft() {
+    $('.mid_nav').animate({
+      left: '350px'
+    }, 400, 'easeOutQuint'); 
+  }
+  
+  function BackToOrigin(){
+    $("#navigation li a").removeClass("open");
+    $('#mainpage').animate({
+      left: '0px'
+    }, 400, 'easeOutQuint');  
+  }
+
 });
